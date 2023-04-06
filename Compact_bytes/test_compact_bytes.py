e7cd0df881f17a5c5e8a13441bd19707f9d2f6c0
EduardoGFilho <121968920+EduardoGFilho@users.noreply.github.com>
2023-04-06 09:18:39 -0300

    Commit of the actual functions

---
+++ Compact_bytes/test_compact_bytes.py
@@ -0,0 +1,20 @@
+import numpy as np
+from compact_bytes_v3 import *
+from numpy.random import randint
+import matplotlib.pyplot as plt
+
+n = 5 # Minimum number of bits to represent the numbers, n<8
+x = randint(low = 0, high = 2**n, size = 100, dtype = np.uint8)
+print(type(x))
+compressed = compact_bytes(x, n)
+uncompressed = decompact_bytes(compressed, n)
+
+print("Original list: ", x)
+print("Decompacted list: ", uncompressed)
+plt.subplot(211)
+plt.ylabel("Original list")
+plt.stem(x)
+plt.subplot(212)
+plt.ylabel("Decompacted list")
+plt.stem(uncompressed)
+plt.show()
